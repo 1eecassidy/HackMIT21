15f6eb673bb2873e2ca70a6c71ea9482d8075eb6
YashAndonia <yashnaik2406@gmail.com>
2021-09-19 00:03:18 -0700

    backend apis made

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -10,21 +10,68 @@ router.route("/get").get(async (req, res) => {
   res.json(result);
 });
 
-router.route("/getAll").get(async (req, res) => {
+router.route("/showFeed").get(async (req, res) => {
   const { filter } = req.body;
   const result = await ItemService.getAll(filter);
   res.json(result);
 });
 
-router.route("/create").post(async (req, res) => {
+router.route("/bidForItem").post(async (req, res) => {
   const { data } = req.body;
-  const result = await ItemService.create(data);
+  const { userId, bid, itemId } = data;
+  if (userId === null || bid === null) {
+    res.status(400).json({ error: "userId,bid value missing" });
+  }
+  const result = await ItemService.addItemToCart(userId, itemId, bid);
+  res.json(result);
+});
+
+router.route("/createItem").post(async (req, res) => {
+  const { data } = req.body;
+  const {
+    userId, //reqd
+    name, //reqd
+    description,
+    size,
+    imageUrl,
+    endDate, //reqd
+    category,
+    itemCondition,
+    startBid, //reqd
+  } = data;
+
+  if (
+    userId === null ||
+    startBid === null ||
+    endDate === null ||
+    name === null
+  ) {
+    res.status(400).json({ error: "userId,bid value missing" });
+  }
+  const result = await ItemService.createItem(
+    userId,
+    name,
+    description,
+    size,
+    imageUrl,
+    endDate,
+    category,
+    itemCondition,
+    startBid
+  );
   res.json(result);
 });
 
 router.route("/update").post(async (req, res) => {
   const { filter, data } = req.body;
   const result = await ItemService.update(filter, data);
+  res.json(result);
+});
+
+router.route("/showCart").post(async (req, res) => {
+  const { filter } = req.body;
+  const { userId } = filter;
+  const result = await ItemService.showCart(userId);
   res.json(result);
 });
 
